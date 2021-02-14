115455f5c88d76796fa97b6a7bfb6cbbda905835
Elisabeth Thompson <thomp2005@gmail.com>
2021-02-14 20:40:18 +0000

    End of Part 2 with issues

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,30 +3,37 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = '';
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let question = 'Who was the first American woman in space? ';
+let correctAnswer = 'Sally Ride';
+let candidateAnswer = '';
+let questions = ["Who was the first American woman in space?", "True or false: 5000 meters = 5 kilometers.', '(5 + 3)/2*10= ?", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?", "What is the minimum crew size for the ISS?"];
+let correctAnswers = ['Sally Ride', 'True', '40', 'Trajectory', '3'];
+let candidateAnswers = [];
+
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+  candidateName = input.question('Enter your name: ');
 }
+
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
-
+for (let i = 0; i < questions.length; i++){
+  candidateAnswers[i] = input.question(questions[i]);
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+if (candidateAnswer[i] === correctAnswers[i]){
+  console.log('Congratulations! You are correct!');
+} else {
+  console.log(`Sorry, ${candidateAnswers[i]} is not the correct answer. ${correctAnswers[i]} is the correct answer.`);
+}
 
 
   let grade;
@@ -38,6 +45,8 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
+  
+console.log(`Greetings ${candidateName}!`);
   
   askQuestion();
   gradeQuiz(this.candidateAnswers);
